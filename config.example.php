bc369c405918b63d33ec76df91bf149adf35a75d
Andrew Kramer <andrew@jmpesp.org>
2019-03-16 18:02:51 +0000

    clean up CDN URL config

--- config.example.php
+++ config.example.php
@@ -30,4 +30,11 @@ $tmp_dir = "/dev/shm";
 $recaptcha_secret = "CHANGEME";
 $recaptcha_sitekey = "CHANGEME";
 
-$ace_url = "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.3/ace.js";
+$cdn = array(
+	"ace_js"		=> "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.3/ace.js",
+	"recaptcha_js"	=> "https://www.google.com/recaptcha/api.js",
+	"jquery_js"		=> "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",
+	"popper_js"		=> "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+	"bootstrap_js"	=> "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js",
+	"bootstrap_css"	=> "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css",
+);
--- index.php
+++ index.php
@@ -30,11 +30,11 @@ if(!isset($_GET["page"]) || !in_array($_GET["page"], $pages)) {
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src='https://www.google.com/recaptcha/api.js'></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<!--	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script> -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script src="<?php echo $cdn["recaptcha_js"]; ?>"></script>
+	<link rel="stylesheet" href="<?php echo $cdn["bootstrap_css"]; ?>">
+	<script src="<?php echo $cdn["jquery_js"]; ?>"></script>
+<!--	<script src="<?php echo $cdn["popper_js"]; ?>"></script> -->
+	<script src="<?php echo $cdn["bootstrap_js"]; ?>"></script>
 	<link rel="stylesheet" href="/css/default.css" />
 </head>
 
